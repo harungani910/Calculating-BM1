4f55615978f8bdb9e8b99907816d54b4e23e484f
harungani910 <harungani910@gmail.com>
2019-11-15 02:20:42 -0600

    first

---
+++ coding.js
@@ -0,0 +1,15 @@
+//mass in kg
+var markMass = 80;
+//height in meters
+var markHeight = 1.80;
+//mass in kg
+var JohnMass = 75;
+//height in meters
+var johnHeight = 1.75;
+// caluculating BMI
+var markBMI = markMass / (markHeight * markHeight);
+var johnBMI = JohnMass / (johnHeight * johnHeight);
+// statement to determine which BMI is higher
+var markhiegherBM1 = markBMI > johnBMI;
+console.log(markhiegherBM1);
+
